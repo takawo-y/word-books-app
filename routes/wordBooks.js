08d9dadcd893dc91b1d22c246693d9ff03aa8116
t-yamamoto <t-yamamoto@itsol.co.jp>
2014-06-19 16:19:10 +0900

    findbyidを実装

--- routes/wordBooks.js
+++ routes/wordBooks.js
@@ -3,13 +3,23 @@
  */
 var express = require('express');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID
 var model = require('../models/WordBook');
 
-/* GET users listing. */
+/** 単語帳全件検索 **/
 router.get('/', function(req, res) {
 	model.find(function(err, wordbooks){
 //		res.json(wordbooks);
 		res.render('wordBookList',{title: 'Word Book List', wordbooks: wordbooks});
+	});
+});
+
+/** ObjectId検索 **/
+router.get('/:id', function(req, res) {
+	var objectId = ObjectID.createFromHexString(req.params.id);
+	model.findById(objectId, function(err, wordbook){
+		res.json(wordbook);
+//		res.render('wordBookList',{title: 'Word Book List', wordbooks: wordbooks});
 	});
 });
 
